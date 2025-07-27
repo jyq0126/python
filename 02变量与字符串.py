128617c786b989b00ea1759c48116f7d0b9bbddb
jyq0126 <3159553288@qq.com>
2025-07-27 21:13:58 +0800

    Add files via upload
    02课时

---
+++ 02变量与字符串.py
@@ -0,0 +1,31 @@
+# x = 3
+# print(x)
+# loveyou1314 = 1314
+# 520baby = 520   SyntaxError: invalid decimal literal
+# 幸运数 = 888
+# print(幸运数)
+# name = '简烨青'
+# print(name)
+# name = '简dog'
+# print(name)
+# x = 3
+# y = 5
+# y = x
+# print(y)
+# x = y = 3
+# print(x)
+# print(y)
+# x = 3
+# y = 5
+# z = x
+# x = y
+# y = z
+# print(x , y)
+# x = 3
+# y = 5
+# x , y = y , x
+# print(x , y)
+# print('I love China')
+# print('I love FishC')
+# print('Let's go')   SyntaxError: unterminated string literal
+print('Let\'s go')
